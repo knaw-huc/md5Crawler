c96f3245eb241684230cea262d97131430b19c55
meindertkroese <>
2021-01-18 14:47:01 +0100

    calculate md5 and write to csv

--- src/main/java/main/java/nl/knaw/huc/md5Crawler/Main.java
+++ src/main/java/main/java/nl/knaw/huc/md5Crawler/Main.java
@@ -1,6 +1,9 @@
 package main.java.nl.knaw.huc.md5Crawler;
 
 
+import com.twmacinta.util.MD5;
+
+import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -23,12 +26,24 @@ public class Main {
         ArrayList allFiles = new ArrayList();
         Files.walk(Paths.get(directory))
              .forEach(allFiles::add);
-        for (Iterator iter = allFiles.iterator(); iter.hasNext(); ) {
-            System.err.println(iter.next().toString());
-        }
 
         System.err.println("compute md5 for all files");
         System.err.println("write results to csv");
+        String outputFilename = "listHash.csv";
+        FileWriter myWriter = new FileWriter(outputFilename);
+
+        for (Iterator iter = allFiles.iterator(); iter.hasNext(); ) {
+            String filename = iter.next().toString();
+            System.err.println(filename);
+            if(!Files.isDirectory(Paths.get(filename))) {
+                String hash = MD5.asHex(MD5.getHash(new java.io.File(filename)));
+                myWriter.write("\"" + filename + "\",\"" + hash + "\"\n");
+                myWriter.flush();
+            } else {
+                System.err.println("is dir?");
+            }
+        }
+        myWriter.close();
     }
 
 }
