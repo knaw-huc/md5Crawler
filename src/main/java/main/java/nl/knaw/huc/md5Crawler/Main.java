c45f0a3ed5fbc6785fb3217ab906a936c757961f
meindertkroese <>
2021-01-19 14:44:01 +0100

    sent filelist stream directly to calc md5 and write result to out. To avoid size issues

--- src/main/java/main/java/nl/knaw/huc/md5Crawler/Main.java
+++ src/main/java/main/java/nl/knaw/huc/md5Crawler/Main.java
@@ -5,19 +5,19 @@ import com.twmacinta.util.MD5;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    private static PrintWriter myWriter;
 
+    public static void main(String[] args) throws Exception {
         String directory = ".";
         String outputFilename = "";
-
         OptionParser parser = new OptionParser("d:o:?*");
         OptionSet options = parser.parse(args);
         if(options.has("d")) {
@@ -26,25 +26,27 @@ public class Main {
         if(options.has("o")) {
            outputFilename = (String)options.valueOf("o");
         }
-
-        ArrayList allFiles = new ArrayList();
-        Files.walk(Paths.get(directory))
-             .forEach(allFiles::add);
-
-        PrintWriter myWriter = null;
+        myWriter = null;
         if(outputFilename!="") {
             myWriter = new PrintWriter(outputFilename);
         } else {
             myWriter = new PrintWriter(System.out);
         }
-        for (Iterator iter = allFiles.iterator(); iter.hasNext(); ) {
-            String filename = iter.next().toString();
-            if(!Files.isDirectory(Paths.get(filename))) {
-                String hash = MD5.asHex(MD5.getHash(new java.io.File(filename)));
-                myWriter.println("\"" + filename.replace("\"","\"\"") + "\",\"" + hash + "\"");
-            }
-        }
+        Files.walk(Paths.get(directory))
+             .forEach(Main::calcAndPrint);
         myWriter.close();
+    }
+
+    public static void calcAndPrint(Path filename) {
+        if(!Files.isDirectory(filename)) {
+            String hash = null;
+            try {
+                hash = MD5.asHex(MD5.getHash(new java.io.File(String.valueOf(filename))));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            myWriter.println("\"" + String.valueOf(filename).replace("\"","\"\"") + "\",\"" + hash + "\"");
+        }
     }
 }
 
